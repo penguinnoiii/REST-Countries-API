41f760aa27b6d6307be88cad098250e237b94e97
penguinnoiii <Pornnapatpoonpolsub@gmail.com>
2025-01-11 19:12:58 +0700

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // All HTML files in the root directory
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
